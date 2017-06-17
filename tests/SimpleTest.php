121b4e31db6b002387a001c9b999ae582b60b37b
Luis Fernando do Nascimento <luisfn@gmail.com>
2017-06-16 23:59:03 -0300

    Setting up unit testing

---
+++ tests/SimpleTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+
+    public function testFirstMethod()
+    {
+        $this->assertEquals('1','1', 'Uhuuuu!');
+    }
+
+}
